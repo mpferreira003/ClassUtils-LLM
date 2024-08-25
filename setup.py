e788873e7b4f19eef47cb9c5f26b4fda1a516d69
Miguel <miguelprates.ferreira@gmail.com>
2024-08-25 12:13:32 -0300

    readme ok

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from setuptools import setup, find_packages
+from pkg_resources import parse_requirements
+
+# Parse requirements from requirements.txt
+with open('requirements.txt') as f:
+    requirements = list(parse_requirements(f))
+
+# Extraindo as strings de dependências
+install_requires = [str(req) for req in requirements]
+
 
 setup(
-    name="RCL",
+    name="ClassUtils-LLM",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[
-        "numpy",
-        "openai",
-        "sentence-transformers",
-        "tensorflow",
-        "matplotlib",
-        "scikit-learn"
-    ],
+    install_requires=install_requires,
     author="Miguel Prates",
     author_email="miguelprates.ferreira@gmail.com",
     description="Biblioteca para análise de risco usando LLM",
