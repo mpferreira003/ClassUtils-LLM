c2d77865bcaec7fa7b5513a274ebf9b9bced1b8f
Miguel <miguelprates.ferreira@gmail.com>
2024-08-25 13:27:34 -0300

    plotter atualizado

--- CULLM/plotter.py
+++ CULLM/plotter.py
@@ -5,24 +5,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot(X,y,ax,title,gt=None,score=adjusted_rand_score):
+def plot(X,y=None,title=None,ax=None,gt=None,score=adjusted_rand_score):
   """
   Function to plot the embeddings
   
   Args:
     X: np.ndarray - embeddings
-    y: np.ndarray - true labels
-    ax: matplotlib axes - plot axes
-    title: str - plot title
+    y: np.ndarray [default=None] - true labels. By default, it will be 0
+      for every embedding.
+    title: str [default=None] - plot title. By default, it will not displayed.
+    ax: matplotlib axes [default=None] - plot axes. If not specified, 
+      it will use matplotlib default plot.
     gt: np.ndarray [default=None] - ground truth labels
     score: callable [default=adjusted_rand_score] - metric to calculate score
   """ 
-  ax.set_title(title)
-  if gt is not None:
-    score = score(gt,y)
-    ax.annotate('{:.2}'.format(score), xy=(0.8, 1), xytext=(0, 3),
-              xycoords='axes fraction', textcoords='offset points',
-              ha='center', va='bottom')
-  for label in np.unique(y):
-    idxs = np.where(label==y)
-    ax.scatter(X[idxs,0],X[idxs,1],alpha=0.5)
+  if y is None:
+    y = np.zeros_like(X[:,0])  # set default labels to 0 for all embeddings
+  
+  if ax is None:
+    if title is not None:
+      ax.set_title(title)
+    if gt is not None:
+      score = score(gt,y)
+      ax.annotate('{:.2}'.format(score), xy=(0.8, 1), xytext=(0, 3),
+                xycoords='axes fraction', textcoords='offset points',
+                ha='center', va='bottom')
+    for label in np.unique(y):
+      idxs = np.where(label==y)
+      ax.scatter(X[idxs,0],X[idxs,1],alpha=0.5)
+  else:
+    if title is not None:
+      plt.set_title(title)
+    if gt is not None:
+      score = score(gt,y)
+      plt.annotate('{:.2}'.format(score), xy=(0.8, 1), xytext=(0, 3),
+                xycoords='axes fraction', textcoords='offset points',
+                ha='center', va='bottom')
+    for label in np.unique(y):
+      idxs = np.where(label==y)
+      plt.scatter(X[idxs,0],X[idxs,1],alpha=0.5)
