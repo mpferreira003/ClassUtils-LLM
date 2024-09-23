4c80ed4705227d7bbcb0c9db242e6ac4c3d73401
Miguel <miguelprates.ferreira@gmail.com>
2024-09-23 20:29:15 -0300

    hotfix tasks

--- ClassUtilsLLM/llm/tasks.py
+++ ClassUtilsLLM/llm/tasks.py
@@ -4,13 +4,13 @@ File that controls que texts used to send prompts to the llm
 
 
 ## taxonomy prompts
-txt_get_taxonomy = lambda documents,n_taxonomy: '@DOCUMENTS\n{documents}\n\n@TASK: Generate the top-{n_taxonomy} topics using the content from @DOCUMENTS. The output must be listed and enumerated.'
+txt_get_taxonomy = lambda documents,n_taxonomy: f'@DOCUMENTS\n{documents}\n\n@TASK: Generate the top-{n_taxonomy} topics using the content from @DOCUMENTS. The output must be listed and enumerated.'
 
 
 ## resume prompts
-txt_resume_2merge = lambda n_taxonomy: 'Merge the context from @TAXONOMY into a top-{n_taxonomy} list. The output must be listed and enumerated.'
-txt_resume_2common = lambda n_taxonomy: 'create a top-{n_taxonomy} list with the most-common items in @TAXONOMY. The output must be listed and enumerated.'
-txt_resume_2severer = lambda n_taxonomy: 'create a top-{n_taxonomy} list with the severer items in @TAXONOMY. The output must be listed and enumerated.'
+txt_resume_2merge = lambda n_taxonomy: f'Merge the context from @TAXONOMY into a top-{n_taxonomy} list. The output must be listed and enumerated.'
+txt_resume_2common = lambda n_taxonomy: f'create a top-{n_taxonomy} list with the most-common items in @TAXONOMY. The output must be listed and enumerated.'
+txt_resume_2severer = lambda n_taxonomy: f'create a top-{n_taxonomy} list with the severer items in @TAXONOMY. The output must be listed and enumerated.'
 
 
 ## context prompts
