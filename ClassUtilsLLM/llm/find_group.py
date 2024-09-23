54f8c639664935db115deed45270d17e5ca0ef32
Miguel <miguelprates.ferreira@gmail.com>
2024-09-23 17:44:23 -0300

    findgroup atualizado

--- ClassUtilsLLM/llm/find_group.py
+++ ClassUtilsLLM/llm/find_group.py
@@ -1,5 +1,5 @@
 from .tasks import txt_find_group
-def itBelongs(doc,groups,llm_query,LLM_MAXCHAR_PER_REQ=None):
+def itBelongs(doc,groups,llm_query,LLM_MAXCHAR_PER_REQ=None,task=None):
   """
   Extract top-n taxonomy from a list of documents
   
@@ -7,14 +7,19 @@ def itBelongs(doc,groups,llm_query,LLM_MAXCHAR_PER_REQ=None):
     doc:str - a document
     groups:list[str] - os grupos ao qual o documento pode pertencer
     llm_query:func(str) - function that communicates with API
+    task:str [default = None] - the task to find the group of the document.
   Returns:
     output:str - llm's answer
   """
   
+  if task is None:
+    task_txt = txt_find_group
+  else:
+    task_txt = task
   ## Construção da pergunta para a llm
   llm_question = f"@DOCUMENT\n{doc}\n" + \
                  f"@GROUP\n{'\n'.join([f'{i} - {groups[i]}' for i in range(len(groups))])}" + \
-                 "\n\n===\n" + txt_find_group
+                 "\n\n===\n" + task_txt
   
   ## Extração da taxonomia
   output = llm_query(llm_question)
